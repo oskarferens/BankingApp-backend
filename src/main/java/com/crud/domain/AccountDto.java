745c904ecbf984bb859690e8cb3d29e9aa6d0cba
Oskar Ferens <oskar.ferens1@gmail.com>
2024-03-20 00:08:02 +0100

    domain classes created

---
+++ src/main/java/com/crud/domain/AccountDto.java
@@ -0,0 +1,9 @@
+package com.crud.domain;
+
+public class AccountDto {
+    /*     Account number (primary key)
+    Customer ID (foreign key)
+    Balance
+    Account type (e.g., savings, current, credit)
+    Account opening date */
+}
